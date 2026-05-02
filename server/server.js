c0f90a830bf0731b426922dfa14d2f5ceaea2921
jerandFin <jerand.lau@gmail.com>
2026-05-01 19:13:46 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public")); 
